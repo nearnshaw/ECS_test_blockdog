29efbd6275130b790966a63770c98e473f8143e9
Nico E <earnshaw.nico@gmail.com>
2018-11-07 18:28:04 -0300

    first attempt

--- src/game.ts
+++ src/game.ts
@@ -1,51 +1,222 @@
-/// --- Set up a system ---
+const camera = Camera.instance
 
-class RotatorSystem extends System {
-  // this group will contain every entity that has a Transform component
-  group = engine.getComponentGroup(Transform)
+enum Goal
+{
+  Idle,
+  Sit,
+  Follow,
+  GoDrink,
+  Drinking,
+}
 
+////////////////////////
+// Custom components
+
+@Component('behavior')
+export class Behavior { 
+    goal: Goal = Goal.Idle
+    previousGoal: Goal = Goal.Idle
+    animationWeight: number = 1
+    timer: number = 100
+}
+
+@Component('walkTarget')
+export class WalkTarget { 
+    target: Vector3 = Vector3.Zero()
+    previousPos: Vector3 = Vector3.Zero()
+    fraction: number = 0
+}
+
+///////////////////////////
+// Entity groups
+
+const dogs = engine.getComponentGroup(Transform, Behavior, WalkTarget)
+
+
+///////////////////////////
+// Systems
+
+
+export class SwitchGoals {
   update(dt: number) {
-    // iterate over the entities of the group
-    for (let entity of this.group.entities) {
-      // get the Transform component of the entity
-      const transform = entity.get(Transform)
+    for (let dog of dogs.entities) {
+      let transform = dog.get(Transform)
+      let behavior = dog.get(Behavior)
+      let walkTarget = dog.get(WalkTarget)
+      behavior.animationWeight += 0.01
+      behavior.timer -= 1
+      if (behavior.timer < 0 ){
+        behavior.timer = 60
+        switch(behavior.goal){
+          case Goal.Idle:
+            considerGoals([
+              {goal: Goal.Sit, odds: .1},
+              {goal: Goal.Follow, odds: .9},
+            ]);
+          case Goal.Drinking:
+            considerGoals([
+              {goal: Goal.Sit, odds: .1},
+            ]);
+          case Goal.Follow:
+            considerGoals([
+              {goal: Goal.Idle, odds: .1},
+            ]);
+          case Goal.GoDrink:
+          case Goal.Sit:
+            considerGoals([
+              {goal: Goal.Idle, odds: .1},
+            ]);
+        } 
+      
 
-      // mutate the rotation
-      transform.rotation.y += dt * 10
+        switch(behavior.goal)
+        {
+          case Goal.Follow:
+            if(!isInBounds(camera.position)) {
+              setDogGoal(Goal.Idle)
+            }
+            else{
+              walkTarget.target = camera.position
+              walkTarget.previousPos = transform.position
+              walkTarget.fraction = 0
+            }
+          case Goal.GoDrink:
+            walkTarget.target = bowl.get(Transform).position
+            walkTarget.previousPos = transform.position
+            walkTarget.fraction = 0
+        }
+      }
     }
   }
 }
 
-// Add a new instance of the system to the engine
-engine.addSystem(new RotatorSystem())
-
-/// --- Spawner function ---
-
-function spawnCube(x: number, y: number, z: number) {
-  // create the entity
-  const cube = new Entity()
-
-  // set a transform to the entity
-  cube.set(new Transform({ position: new Vector3(x, y, z) }))
-
-  // set a shape to the entity
-  cube.set(new BoxShape())
-
-  // add the entity to the engine
-  engine.addEntity(cube)
-
-  return cube
+export class walk {
+  update(dt: number) {
+    for (let dog of dogs.entities) {
+      let transform = dog.get(Transform)
+      let walk = dog.get(WalkTarget)
+      if (walk.fraction < 1){
+        transform.position = Vector3.Lerp(walk.previousPos, walk.target, walk.fraction)
+        walk.fraction += 1/30
+  
+      }
+    }
+  }
 }
 
-/// --- Spawn a cube ---
 
-const cube = spawnCube(5, 1, 5)
+engine.addSystem(new SwitchGoals())
+engine.addSystem(new walk())
 
-cube.set(
-  new OnClick(() => {
-    cube.get(Transform).scale.z *= 1.1
-    cube.get(Transform).scale.x *= 0.9
+///////////////////////////
+// INITIAL ENTITIES
 
-    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
-  })
-)
+
+// Bowl
+const bowl = new Entity()
+bowl.set(new GLTFShape("models/BlockDogBowl.gltf"))
+bowl.set(new Transform())
+bowl.get(Transform).position.set(9, 0, 1)
+bowl.set(new OnClick( _ => {
+    setDogGoal(Goal.Drinking)
+}))
+engine.addEntity(bowl)
+
+// Garden
+const garden = new Entity()
+garden.set(new GLTFShape("models/garden.gltf"))
+garden.set(new Transform())
+garden.get(Transform).position.set(5, 0, 5)
+engine.addEntity(garden)
+
+
+// Dog
+const dog = new Entity()
+dog.set(new GLTFShape("models/BlockDog.gltf"))
+dog.set(new Transform())
+dog.get(Transform).position.set(5, 0, 5)
+dog.set(new Behavior())
+dog.set(new WalkTarget())
+dog.set(new OnClick( _ => {
+  if (dog.get(Behavior).goal == Goal.Sit)
+  {
+    setDogGoal(Goal.Idle)
+  }
+  else {
+    setDogGoal(Goal.Sit)
+  }
+}))
+engine.addEntity(dog)
+
+
+////////////////////////
+//OTHER FUNCTIONS
+
+
+function isInBounds(position: Vector3): boolean
+{
+  return position.x > .5 && position.x < 9.5
+    && position.z > .5 && position.z < 9.5;
+}
+
+function setDogGoal(goal: Goal){
+  let behavior = dog.get(Behavior)
+  behavior.previousGoal = behavior.goal  //is this a reference?
+  behavior.goal = goal
+  behavior.animationWeight = 1 - behavior.animationWeight
+  log("new goal: " + goal)
+}
+
+
+
+function considerGoals(goals: {goal: Goal, odds: number}[]) {
+  for(let i = 0; i < goals.length; i++)
+  {
+    if(Math.random() < goals[i].odds)
+    {
+      switch(goals[i].goal)
+      {
+        case Goal.Follow:
+          if(!isInBounds(camera.position))
+          {
+            continue;
+          }
+      }
+
+      setDogGoal(goals[i].goal);
+      return;
+    }
+  }
+}
+
+
+
+// function getAnimationRates() : {idle: number, sit: number, walk: number} {
+//   const weight = Math.min(Math.max(dog.get(Behavior).animationWeight, 0), 1);
+//   const inverse = 1 - weight;
+
+//   let sit = 0;
+//   let walk = 0;
+  
+//   switch(dog.get(Behavior).previousGoal){
+//     case Goal.Sit:
+//       sit = inverse;
+//       break;
+//     case Goal.Follow:
+//     case Goal.GoDrink:
+//       walk = inverse;
+//       break;
+//   }
+
+//   switch(dog.get(Behavior).goal){
+//     case Goal.Sit:
+//       sit = weight;
+//       break;
+//     case Goal.Follow:
+//     case Goal.GoDrink:
+//       walk = weight;
+//       break;
+//   }
+
+//   return {idle: 1 - (sit + walk), sit, walk};
+// }
